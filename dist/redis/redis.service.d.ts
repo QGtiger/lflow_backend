250f0f56ebcfbce60062f52c55d14b3af5397d6a
光鱼 <guang.yu@yingdao.com>
2024-08-28 20:39:26 +0800

    feat: 33

--- dist/redis/redis.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class RedisService {
-    private redisClient;
-    get(key: string): Promise<string>;
-    set(key: string, value: string, ttl?: number): Promise<void>;
-    del(key: string): Promise<void>;
-}
